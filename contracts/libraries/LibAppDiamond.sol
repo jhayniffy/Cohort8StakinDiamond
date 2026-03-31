a9e68f40efefd0e4fef5f9ac6f34b2caedfb64fb
Jeremiah Peter <jeremiahniffypeter@gmail.com>
2026-03-31 23:31:10 +0100

    feat:add

---
+++ contracts/libraries/LibAppDiamond.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+library LibAppStorage {
+    struct Layout{
+        uint256 count;
+        
+    }
+}
---
+++ test/deployDiamond.t.sol
@@ -0,0 +1,99 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.0;
+
+import "../contracts/interfaces/IDiamondCut.sol";
+import "../contracts/facets/DiamondCutFacet.sol";
+import "../contracts/facets/DiamondLoupeFacet.sol";
+import "../contracts/facets/OwnershipFacet.sol";
+import "../contracts/facets/IncreaseCount.sol";
+import "../contracts/libraries/LibAppDiamond.sol";
+import "forge-std/Test.sol";
+import "../contracts/Diamond.sol";
+
+contract DiamondDeployer is Test, IDiamondCut {
+    //contract types of facets to be deployed
+    Diamond diamond;
+    DiamondCutFacet dCutFacet;
+    DiamondLoupeFacet dLoupe;
+    OwnershipFacet ownerF;
+    IncreaseCount incCount;
+
+    function setUp() public {
+        //deploy facets
+        dCutFacet = new DiamondCutFacet();
+        diamond = new Diamond(address(this), address(dCutFacet));
+        dLoupe = new DiamondLoupeFacet();
+        ownerF = new OwnershipFacet();
+
+        //upgrade diamond with facets
+
+        //build cut struct
+        FacetCut[] memory cut = new FacetCut[](2);
+
+        cut[0] = (
+            FacetCut({
+                facetAddress: address(dLoupe),
+                action: FacetCutAction.Add,
+                functionSelectors: generateSelectors("DiamondLoupeFacet")
+            })
+        );
+
+        cut[1] = (
+            FacetCut({
+                facetAddress: address(ownerF),
+                action: FacetCutAction.Add,
+                functionSelectors: generateSelectors("OwnershipFacet")
+            })
+        );
+
+        //upgrade diamond
+        IDiamondCut(address(diamond)).diamondCut(cut, address(0x0), "");
+
+        //call a function
+        DiamondLoupeFacet(address(diamond)).facetAddresses();
+    }
+
+    function testInDiamondLoupeFacetcreaseCount() public {
+        //deploy IncreaseCount facet
+        incCount = new IncreaseCount();
+
+        //build cut struct
+        FacetCut[] memory cut = new FacetCut[](1);
+
+        cut[0] = (
+            FacetCut({
+                facetAddress: address(incCount),
+                action: FacetCutAction.Add,
+                functionSelectors: generateSelectors("IncreaseCount")
+            })
+        );
+
+        //upgrade diamond
+        IDiamondCut(address(diamond)).diamondCut(cut, address(0x0), "");
+
+        //call increaseCount function
+        IncreaseCount(address(diamond)).increaseCount(5);
+
+        //check if count was increased
+        uint count = IncreaseCount(address(diamond)).getCount();
+
+        assertEq(count, 5);
+    }
+
+    function generateSelectors(
+        string memory _facetName
+    ) internal returns (bytes4[] memory selectors) {
+        string[] memory cmd = new string[](3);
+        cmd[0] = "node";
+        cmd[1] = "scripts/genSelectors.js";
+        cmd[2] = _facetName;
+        bytes memory res = vm.ffi(cmd);
+        selectors = abi.decode(res, (bytes4[]));
+    }
+
+    function diamondCut(
+        FacetCut[] calldata _diamondCut,
+        address _init,
+        bytes calldata _calldata
+    ) external override {}
+}
